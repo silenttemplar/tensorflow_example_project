18b26e6b877e0e4104b3dedfacf1ef98887d84bd
sjyoon <silent6578@gmail.com>
2020-12-28 16:45:22 +0900

    프로젝트 dir 구조 변경

--- serial_training_LeNet5.py
+++ serial_training_LeNet5.py
@@ -1,12 +1,12 @@
 import tensorflow as tf
 
 from utils.learning_env_setting import dir_setting, continue_setting, get_classification_metrics, argparser
-from utils.dataset_utils import load_processing_mnist, load_processing_cifar10
+from utils.dataset_utils import load_processing_mnist
 from utils.train_validation_test import train, validation, test
 from utils.checkpoint_utils import save_metrics_model, metric_visualizer
 from utils.basic_utils import resetter, training_report
 
-from example.leNet_impl_with_subclassing import LeNet5
+from example.tensorflow2.leNet_impl_with_subclassing import LeNet5
 
 ''' === Learning Setting === '''
 CONTINUE_LEARNING = True
@@ -23,12 +23,14 @@ exp_idx, epochs, learning_rate, train_batch_size, activation = argparser(epochs=
                                                                          train_batch_size=train_batch_size)
 
 exp_name = 'exp{}_{}_LeNet5'.format(exp_idx, 'learning_rate')
+base_dir = 'tensorflow2/'+exp_name
+
 model = LeNet5(activation=activation)
 optimizer = tf.keras.optimizers.SGD(learning_rate=learning_rate)
 ''' === Learning Setting === '''
 
 loss_object = tf.keras.losses.SparseCategoricalCrossentropy()
-path_dict = dir_setting(exp_name, CONTINUE_LEARNING)
+path_dict = dir_setting(base_dir, CONTINUE_LEARNING)
 model, losses_accs, start_epoch = continue_setting(CONTINUE_LEARNING, path_dict, model=model)
 
 train_ds, validation_ds, test_ds = load_processing_mnist(train_ratio, train_batch_size, test_batch_size)
